9eb6ee260b38e6dd277e4aa9b00f783f011e1dc6
ledesmx <alberto.ledesma.dev@gmail.com>
2025-12-08 21:08:11 -0600

    add about page

---
+++ resources/views/about.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About page</title>
+</head>
+<body>
+    <h1>Hello from the about page</h1>
+</body>
+</html>
