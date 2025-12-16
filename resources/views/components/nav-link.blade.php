ddf205ebf33039c3cee0bd766a5cacfd56ef5a59
ledesmx <alberto.ledesma.dev@gmail.com>
2025-12-15 19:54:46 -0600

    add nav link anchor tag variation style

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,6 +1,13 @@
-@props(['active' => false])
+@props(['active' => false, 'type' => "button"])
 
-<a class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium"
-{{ $attributes }}>
-{{ $slot }}
-</a>
+@if ($type == "button")
+    <a class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium"
+    {{ $attributes }}>
+    {{ $slot }}
+    </a>
+@else
+    <a class="{{ $active ? 'text-white underline' : 'text-gray-300 hover:text-white' }} text-sm font-medium"
+    {{ $attributes }}>
+    {{ $slot }}
+    </a>
+@endif
